90f153cc8f1ed5684f82eace2ba0b6d6070cdf62
marinafischer <marinamirandafischer@gmail.com>
2022-06-25 16:36:14 -0300

    task service postTesk tests

---
+++ test/unit/services/taskServicePostTask.test.js
@@ -0,0 +1,49 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { describe, beforeEach, afterEach, it } = require('mocha');
+const taskModel = require('../../../models/taskModel');
+const taskService = require('../../../services/taskService');
+const connection = require('../../../models/connection');
+
+describe('Insere uma nova tarefa, retorna as tarefas do usuário atualizadas', ()=>{
+  describe('Retorna um array de tarefas', ()=>{
+    const user = {
+      id: 5,
+      user_id:1,
+      content: 'um conteúdo qualquer',
+      status: 1
+    };
+    const resultExecute = [[
+      {
+        id: 5,
+        user_id:1,
+        content: 'um conteúdo qualquer',
+        status: 1
+      }
+    ]];
+
+    beforeEach(() => {
+      sinon.stub(taskModel, 'putTask').resolves();
+      sinon.stub(connection, 'execute').resolves(resultExecute);
+    });
+
+    afterEach(() => {
+      taskModel.putTask.restore();
+      connection.execute.restore();
+    });
+    it('retorna um array', async()=>{
+      const data = await taskService.updateTask(user);
+      expect(data).to.be.an('array');
+    });
+    it('o array não está vazio', async()=>{
+      const data = await taskService.updateTask(user);
+      expect(data).to.be.not.empty;
+    });
+    it('os objetos do array possuem atributos "id", "user_id", "content", "status"', 
+      async () => {
+        const [data] = await taskService.updateTask(user);
+        expect(data).to.be.includes.all.keys('id', 'user_id', 'content', 'status');
+      }
+    );
+  });
+})
