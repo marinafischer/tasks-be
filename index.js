09f24bc6586c60645fa9ccbce61640ee748e0a46
marinafischer <marinamirandafischer@gmail.com>
2022-07-01 16:27:31 -0300

    fix: use cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const userRoute = require('./routes/userRoute');
 const tasksRoute = require('./routes/tasksRoute');
 
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(cors());
 
 app.use(express.json());
 
